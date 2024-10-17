80b70698b54731cece752fea947f468e35b38b52
SimonTimi <B00163297@mytudublin.ie>
2024-10-18 00:13:43 +0100

    Added Unit 2 project files including Challenge 2

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float spawnCooldown = 3f;  // Time in seconds between spawns
+
+    private float lastSpawnTime;      // Tracks the last time a dog was spawned
+
+    void Start(){
+        lastSpawnTime = -spawnCooldown; //player can spawn straight away
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time - lastSpawnTime >= spawnCooldown)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            lastSpawnTime = Time.time;  // Update the last spawn time to the current time
+        }
+    }
+}
---
+++ Prototype 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    public float minSpawnTime = 3f;   // Minimum time between spawns (in seconds)
+    public float maxSpawnTime = 5f;   // Maximum time between spawns (in seconds)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Start the spawning coroutine after a short delay
+        Invoke("StartSpawning", startDelay);
+    }
+
+    void StartSpawning()
+    {
+        // Start the coroutine that spawns balls randomly
+        StartCoroutine(SpawnRandomBall());
+    }
+
+    // Coroutine to spawn balls at random intervals
+    IEnumerator SpawnRandomBall()
+    {
+        while (true)  // Continue to spawn indefinitely
+        {
+            // Generate random spawn position and random ball index
+            Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+            int ballIndex = Random.Range(0, ballPrefabs.Length);
+
+            // Instantiate ball at random spawn location
+            Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+
+            // Wait for a random time between minSpawnTime and maxSpawnTime before spawning again
+            float timeToWait = Random.Range(minSpawnTime, maxSpawnTime);
+            yield return new WaitForSeconds(timeToWait);
+        }
+    }
+}
